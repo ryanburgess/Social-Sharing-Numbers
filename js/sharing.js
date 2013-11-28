973454a85c5c16ae4367633156756851d34182d2
Ryan Burgess <rburgess@evernote.com>
2013-11-27 16:36:32 -0800

    added linkedin and pinterest to pull share counts

--- js/sharing.js
+++ js/sharing.js
@@ -1,14 +1,16 @@
 (function($) {
-    "use strict";
-
-    var getUrl = window.location.href;
-    var facebookUrl = 'http://graph.facebook.com/?ids='+getUrl;
-    var twitterUrl = 'http://urls.api.twitter.com/1/urls/count.json?url='+getUrl;
+ 
+    //var getUrl = window.location.href; // use this to get the numbers for page url
+    var getUrl = "http://blog.evernote.com/blog/2013/11/26/how-to-capture-your-social-media-memories-with-evernote/"; // Test url
+    var facebookUrl = "http://graph.facebook.com/?ids="+getUrl;
+    var twitterUrl = "http://urls.api.twitter.com/1/urls/count.json?url="+getUrl;
+    var linkedInUrl = "http://www.linkedin.com/countserv/count/share?url="+ getUrl;
+    var pinterestUrl = "http://api.pinterest.com/v1/urls/count.json?&url="+getUrl;
 	
     //facebook likes/share
     $.ajax({
         url: facebookUrl,
-        dataType: 'jsonp',
+        dataType: "jsonp",
         success: function(data) {
             var likes = data[getUrl].shares;
             $(".facebook .numbers").html(likes);
@@ -18,7 +20,7 @@
     //twitter tweets
     $.ajax({
         url: twitterUrl,
-        dataType: 'jsonp',
+        dataType: "jsonp",
         success: function(data){
             var tweets = data.count;
             $(".twitter .numbers").html(tweets);
@@ -29,11 +31,31 @@
 	$.ajax({
         type: "POST",
         url: "/inc/googleplus.php",
-        data: {'url': getUrl},
+        data: {"url": getUrl},
         cache: false,
-        success: function(result)
-        {	
+        success: function(result){	
             $(".google-plus .numbers").html(result);
+        }
+    });
+
+    //LinkedIn shares
+    $.ajax({
+        url: linkedInUrl,
+        dataType: "jsonp",
+        success: function(data){
+            var shares = data.count;
+            $(".linked-in .numbers").html(shares);
+        }
+    });
+
+    //Pinterest Pins
+    $.ajax({
+        url: pinterestUrl,
+        dataType: "jsonp",
+        success: function(data){
+            console.log(data);
+            var pins = data.count;
+            $(".pinterest .numbers").html(pins);
         }
     });
 	
