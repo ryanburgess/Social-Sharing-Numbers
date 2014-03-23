6b6ce34ada208a2257b9ee7b06a6c5b01c9a6bfb
Ryan Burgess <ryan@senseslost.com>
2014-03-22 23:47:38 -0700

    Update googleplus.php

--- inc/googleplus.php
+++ inc/googleplus.php
@@ -1,6 +1,6 @@
 <?php
 	$url = $_POST['url'];
-	    $buttonUrl = sprintf('https://plusone.google.com/u/0/_/+1/fastbutton?url=%s', urlencode($url));
+	$buttonUrl = sprintf('https://plusone.google.com/u/0/_/+1/fastbutton?url=%s', urlencode($url));
 	$htmlData  = file_get_contents($buttonUrl);
 	
 	@preg_match_all('#{c: (.*?),#si', $htmlData, $matches);
