a36cdb284c5a6a02fb47b10c037fb661f6b6f6ce
Ryan Burgess <ryan@senseslost.com>
2014-03-22 23:47:19 -0700

    clean up googleplus php

--- inc/googleplus.php
+++ inc/googleplus.php
@@ -1,15 +1,12 @@
 <?php
-		$url = $_POST['url'];
-		
-    	$buttonUrl = sprintf('https://plusone.google.com/u/0/_/+1/fastbutton?url=%s', urlencode($url));
-	    $htmlData  = file_get_contents($buttonUrl);
-
-	    @preg_match_all('#{c: (.*?),#si', $htmlData, $matches);
+	$url = $_POST['url'];
+	    $buttonUrl = sprintf('https://plusone.google.com/u/0/_/+1/fastbutton?url=%s', urlencode($url));
+	$htmlData  = file_get_contents($buttonUrl);
+	
+	@preg_match_all('#{c: (.*?),#si', $htmlData, $matches);
 	    $ret = isset($matches[1][0]) && strlen($matches[1][0]) > 0 ? trim($matches[1][0]) : 0;
 	    if(0 != $ret) {
 	        $ret = str_replace('.0', '', $ret);
 	    }
-	   //return intval($ret);
-		echo intval($ret)
-
+	echo intval($ret);
 ?>
